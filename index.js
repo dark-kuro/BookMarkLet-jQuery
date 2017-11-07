43ad039b7e2a466ab975b06808ef648b4721e3fd
dark-kuro <33413554+dark-kuro@users.noreply.github.com>
2017-11-07 19:42:12 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-alert(666);
+(function($) {
+    function injectJs(url) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        document.body.appendChild(script);
+    }
+    var href = location.href;
+    if (/yande\.re/.test(url)) injectJs('https://pastebin.com/raw/iGADYvSu');
+})(window.jQuery);
